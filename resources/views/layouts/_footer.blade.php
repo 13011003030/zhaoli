9e3adf5bbbee7c3044519e4dff5baa9a1bccb47b
zhaoli <zhao-li@126.com>
2017-12-03 05:07:45 +0000

    完成静态页路由注册

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,16 @@
+<div class="col-md-12">
+    <footer class="footer">
+        <small class="slogon">
+            <img class="brand-icon"
+                 src="http://www.zhaoliu.cn/bbs/bbs.ico" style="width: 17px">
+            <a href="http://www.zhaoliu.cn/" target="_blank">
+                赵凯毅 | 健康成长
+            </a>
+        </small>
+        <nav>
+            <ul>
+                <li><a href="{{ route('about') }}">关于</a></li>
+            </ul>
+        </nav>
+    </footer>
+</div>
